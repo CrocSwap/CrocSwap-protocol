72d3bf5a41ab8ec3b08eace7cbd8df81c3d58e22
Douglas R Colkitt <douglas.colkitt@gmail.com>
2021-08-24 16:02:36 +0000

    Add natSpec documentation to LevelBook mixin

--- contracts/mixins/LevelBook.sol
+++ contracts/mixins/LevelBook.sol
@@ -8,8 +8,22 @@ import '../libraries/LiquidityMath.sol';
 import '../libraries/TickMath.sol';
 import './TickCensus.sol';
 
+/* @title Level Book Mixin
+ * @notice Mixin contract that tracks the aggregate liquidity bumps and in-range reward
+ *         accumulators on a per-tick basis. */
 contract LevelBook is TickCensus {
 
+    /* Book level structure exists one-to-one on a tick basis (though could possibly be
+     * zero-valued). For each tick we have to track three values:
+     *    bidLiq_ - The concentrated liquidity bump that's added to the AMM curve when
+     *              the price moves into the tick from above.
+     *    askLiq_ - The concentrated liquidity bump that's added to the AMM curve when
+     *              the price moves into the tick from below.
+     *    feeOdometer_ - The liquidity fee rewards accumulator that's checkpointed 
+     *       whenever the price crosses the tick boundary. Used to calculate the 
+     *       cumulative fee rewards on any arbitrary lower-upper tick range. This is
+     *       generically represented as a per-liquidity unit 128-bit fixed point 
+     *       cumulative growth rate. */
     struct BookLevel {
         uint128 bidLiq_;
         uint128 askLiq_;
@@ -20,11 +34,30 @@ contract LevelBook is TickCensus {
 
     uint16 private tickSize_;
 
+    /* @notice Retrieves the level book state associated with the tick. */
     function levelState (int24 tick) internal view
         returns (BookLevel memory) {
         return levels_[tick];
     }
 
+    /* @notice Called when the curve price moves through the tick boundary. Performs
+     *         the necessary accumulator checkpointing and deriving the liquidity bump.
+     *
+     * @dev    Note that this function call is *not* idempotent. It's the callers 
+     *         responsibility to only call once per tick cross direction. Otherwise 
+     *         behavior is undefined. This is safe to call with non-initialized zero
+     *         ticks but should generally be avoided for gas efficiency reasons.
+     *
+     * @param tick - The 24-bit tick index being crossed.
+     * @param isBuy - If true indicates that price is crossing the tick boundary from 
+     *                 below. If false, means tick is being crossed from above. 
+     * @param feeGlobal - The up-to-date global fee reward accumulator value. Used to
+     *                    checkpoint the tick rewards for calculating accumulated rewards
+     *                    in a range. Represented as 128-bit fixed point cumulative 
+     *                    growth rate per unit of liquidity.
+     *
+     * @return liqDelta - The net change in concentrated liquidity that should be applied
+     *                    to the AMM curve following this level cross. */
     function crossLevel (int24 tick, bool isBuy, uint256 feeGlobal)
         internal returns (int256 liqDelta) {
         BookLevel memory lvl = levels_[tick];
@@ -35,7 +68,23 @@ contract LevelBook is TickCensus {
             levels_[tick].feeOdometer_ = feeGlobal - levels_[tick].feeOdometer_;
         }
     }
-    
+
+    /* @notice Adds the liquidity associated with a new range order into the associated
+     *         book levels, initializing the level structs if necessary.
+     *
+     * @dev This method will enforce the minimum tick spacing constraint by requiring 
+     *      that any upper or lower bound tick index is modulo the current tick size. 
+     * 
+     * @param midTick - The tick index associated with the current price of the AMM curve
+     * @param bidTick - The tick index for the lower bound of the range order.
+     * @param askTick - The tick index for the upper bound of the range order.
+     * @param liq - The amount of liquidity being added by the range order.
+     * @param feeGlobal - The up-to-date global fee rewards growth accumulator. 
+     *    Represented as 128-bit fixed point growth rate.
+     *
+     * @return feeOdometer - Returns the current fee reward accumulator value for the
+     *    range specified by the order. This is necessary, so we consumers of this mixin
+     *    can subtract the rewards accumulated before the order was added. */
     function addBookLiq (int24 midTick, int24 bidTick, int24 askTick, uint128 liq,
                          uint256 feeGlobal)
         internal returns (uint256 feeOdometer) {
@@ -47,12 +96,15 @@ contract LevelBook is TickCensus {
         feeOdometer = clockFeeOdometer(midTick, bidTick, askTick, feeGlobal);
     }
 
+    /* @notice Sets the tick spacing constraint. After being set all new orders will 
+     *    only be allowed to add at tick indices module this set value. Set to 0 to
+     *    allow orders at every tick. */
     function setTickSize (int24 tickSize) internal {
         require(tickSize >= 0 && tickSize < type(uint16).max);
         tickSize_ = uint16(tickSize);
     }
 
-    
+    /* @notice Returns the currently set tick spacing contraint. */
     function getTickSize() internal view returns (uint16) {
         return tickSize_;
     }
@@ -63,7 +115,23 @@ contract LevelBook is TickCensus {
             require(askTick % tickSize_ == 0, "D");
         }
     }
-    
+
+    /* @notice Call when removing liquidity associated with a specific range order.
+     *         Decrements the associated tick levels as necessary.
+     *
+     * @param midTick - The tick index associated with the current price of the AMM curve
+     * @param bidTick - The tick index for the lower bound of the range order.
+     * @param askTick - The tick index for the upper bound of the range order.
+     * @param liq - The amount of liquidity being added by the range order.
+     * @param feeGlobal - The up-to-date global fee rewards growth accumulator. 
+     *    Represented as 128-bit fixed point growth rate.
+     *
+     * @return feeOdometer - Returns the current fee reward accumulator value for the
+     *    range specified by the order. Note that this returns the accumulated rewards
+     *    from the range history, including *before* the order was added. It's the 
+     *    downstream user's responsibility to adjust this value with the odometer clock
+     *    from addBookLiq to correctly calculate the rewards accumulated over the 
+     *    lifetime of the order. */     
     function removeBookLiq (int24 midTick, int24 bidTick, int24 askTick, uint128 liq,
                             uint256 feeGlobal)
         internal returns (uint256 feeOdometer) {
@@ -129,7 +197,14 @@ contract LevelBook is TickCensus {
         return false;
     }    
 
-    
+    /* @notice Calculates the current accumulated fee rewards in a given concentrated
+     *         liquidity tick range. The difference between this value at two different
+     *         times is guaranteed to reflect the accumulated rewards in the tick range
+     *         between those two times.
+     * @dev This returned result only has meaning when compared against the result
+     *      from the same method call on the same range at a different time. Any
+     *      given range could have an arbitrary offset relative to the pool's actual
+     *      cumulative rewards. */
     function clockFeeOdometer (int24 currentTick, int24 lowerTick, int24 upperTick,
                                uint256 feeGlobal)
         internal view returns (uint256) {
@@ -138,6 +213,13 @@ contract LevelBook is TickCensus {
         return feeUpper - feeLower;
     }
 
+    /* @dev Internally we checkpoint the last global accumulator value from the last
+     *      time the level was crossed. Because fees can only accumulate when price
+     *      is in range, the checkpoint represents the global fees that accumulated
+     *      on the outside of the tick level. (Though this may be faked for fees that
+     *      that accumulated prior to level initialization. It doesn't matter, because
+     *      all we use this value for is calculating the delta of fee accumulation 
+     *      between two different post-initialization points in time.) */
     function pivotFeeBelow (int24 lvlTick, int24 currentTick, uint256 feeGlobal)
         private view returns (uint256) {
         BookLevel storage lvl = levels_[lvlTick];
