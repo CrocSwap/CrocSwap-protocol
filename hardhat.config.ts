9bee51d955e6000e5ce111867f3db6456d240fca
Douglas R Colkitt <douglas.colkitt@gmail.com>
2021-09-16 18:04:37 +0000

    Roll to Solidity v0.8. Breaks compile time... Fixes to come in subsequent commits

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,7 +7,7 @@ import "@nomiclabs/hardhat-ethers";
 
 module.exports = {
     solidity: {
-      version: "0.7.6",
+      version: "0.8.4",
       settings: {
         optimizer: {
           enabled: true,
