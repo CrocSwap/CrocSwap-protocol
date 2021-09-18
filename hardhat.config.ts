f6f2e809c09e388871827ccee88f9d5ecce7f86d
Douglas R Colkitt <douglas.colkitt@gmail.com>
2021-09-18 14:01:41 +0000

    Add support for Tenderly

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,7 @@
 
 import "hardhat-typechain";
 import "@nomiclabs/hardhat-ethers";
+import "hardhat-contract-sizer"
 
 module.exports = {
     solidity: {
@@ -14,5 +15,12 @@ module.exports = {
           runs: 1000
         }
       }
-    }
+    },
+
+    networks: {
+       local: {
+         url: 'http://127.0.0.1:8545',
+         chainId: 31337
+       }
+    },
 };
