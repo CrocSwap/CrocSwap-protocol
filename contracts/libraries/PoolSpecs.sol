f8ad8bf74021144aa139dc26c7a6c6bfa93fb118
Douglas R Colkitt <douglas.colkitt@gmail.com>
2021-09-28 01:43:36 +0000

    Extend pool specs for full context

--- contracts/libraries/PoolSpecs.sol
+++ contracts/libraries/PoolSpecs.sol
@@ -3,16 +3,40 @@
 pragma solidity >=0.8.4;
 pragma experimental ABIEncoderV2;
 
-import './LowGasSafeMath.sol';
-
 /* @title Pool specification library */
 library PoolSpecs {
 
-    struct PoolSpec {
+    struct PoolHeader {
         uint24 feeRate_;
         uint8 protocolTake_;
-        uint8 tickSpacing_;
+        uint16 tickSize_;
+        uint8 priceOracle_;
+        uint8 authFlags_;
     }
 
-    uint constant MAX_POOL_COUNT = 256;
+    struct PoolExtended {
+        address authOracle_;
+    }
+    
+    struct PoolSpecs {
+        PoolHeader head_;
+        PoolExtended ext_;
+    };
+
+    
+    function queryPool (mapping(bytes32 => PoolSpecs) store,
+                        address tokenX, address tokenY, uint256 poolIdx)
+        internal view returns (PoolSpecs memory specs) {
+        bytes32 key = encodeKey(tokenX, tokeyY, poolIdx);
+        PoolHeader memory header = store[key].head_;
+        PoolHeader memory ext = needsExt(header) ?
+            store[key].ext_ : emptyExt();
+        return PoolSpecs({header, ext});
+    }
+
+    function encodeKey (address tokenX, address tokeyY, uint256 poolIdx)
+        private pure returns (bytes32) {
+        require(tokenX < tokenY);
+        return keccack256(abi.encode(tokenX, tokenY, poolIdx));
+    }
 }
