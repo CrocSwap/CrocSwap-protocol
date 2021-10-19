708e3e0794403304dacf368bc8cbefde159d1e0f
Douglas R Colkitt <douglas.colkitt@gmail.com>
2021-10-19 12:59:38 +0000

    Fix hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -26,10 +26,10 @@ module.exports = {
        local: {
          url: 'http://127.0.0.1:8545',
          chainId: 31337
-       }
+       },
        ropsten: {
          url: 'https://ropsten.infura.io/v3/cf3bc905d88d4f248c6be347adc8a1d8',
-         chainId: 3,
+         chainId: 3
        }
 
     },
