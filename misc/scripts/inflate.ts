f371431384b92485659f37419b348e5771a51bb3
Douglas R Colkitt <douglas.colkitt@gmail.com>
2021-12-10 19:53:35 +0000

    Hacking around on the informal deploy scripts

--- misc/scripts/inflate.ts
+++ misc/scripts/inflate.ts
@@ -4,7 +4,7 @@ import { ethers } from 'hardhat';
 import { ContractFactory, BytesLike, BigNumber, Signer, ContractTransaction } from 'ethers';
 import { JsonRpcProvider } from '@ethersproject/providers';
 import { toSqrtPrice, fromSqrtPrice, maxSqrtPrice } from '../../test/FixedPoint';
-import { MockERC20 } from '../../typechain/MockERC20';
+import { MockBypassERC20 } from '../../typechain/MockBypassERC20';
 import { QueryHelper } from '../../typechain/QueryHelper';
 import { CrocSwapDex } from '../../contracts/typechain/CrocSwapDex';
 import { minSqrtPrice } from './tmpSwap';
@@ -19,7 +19,7 @@ const BIG_QTY = BigNumber.from("10").pow(30)
 
 export const ZERO_ADDR = "0x0000000000000000000000000000000000000000"
 
-let override = { gasPrice: BigNumber.from("10").pow(9).mul(5), gasLimit: 1000000 }
+let override = { gasPrice: BigNumber.from("10").pow(9).mul(25), gasLimit: 1000000 }
 
 function encodeMintAmbient (base: string, quote: string, pool: number,
     liq: BigNumber, useSurplus: boolean): BytesLike {
@@ -35,22 +35,22 @@ async function waitMine (tx: Promise<ContractTransaction>): Promise<ContractTran
     return tx
 }
 
-async function deployToken (symbol: string, decimals: number): Promise<MockERC20> {
-    let factory = await ethers.getContractFactory("MockERC20")
-    let token = await factory.deploy(override) as MockERC20
+async function deployToken (symbol: string, decimals: number): Promise<MockBypassERC20> {
+    let factory = await ethers.getContractFactory("MockBypassERC20")
+    let token = await factory.deploy(override) as MockBypassERC20
     await waitMine(token.setSymbol(symbol, override))
     await waitMine(token.setDecimals(decimals, override))
     console.log("Token " + symbol + " " + token.address)
     return token
 }
 
-async function fundToken (token: MockERC20, trader: Signer, dex: string): Promise<void> {
+async function fundToken (token: MockBypassERC20, trader: Signer, dex: string): Promise<void> {
     await waitMine(token.deposit(await trader.getAddress(), BIG_QTY, override))
     await waitMine(token.connect(trader).approve(dex, BIG_QTY, override))
     console.log(await token.symbol() + " funded")
 }
 
-async function createPool (token: MockERC20, base: MockERC20, price: number, dex: CrocSwapDex): Promise<void> {
+async function createPool (token: MockBypassERC20, base: MockBypassERC20, price: number, dex: CrocSwapDex): Promise<void> {
     let baseDecs = await base.decimals()
     let quoteDecs = await token.decimals()
     let deflator = Math.pow(10, baseDecs-quoteDecs)
@@ -65,7 +65,7 @@ async function createPool (token: MockERC20, base: MockERC20, price: number, dex
     }
 }
 
-async function mintLiq (token: MockERC20, base: MockERC20, dex: CrocSwapDex, query: QueryHelper): Promise<void> {
+async function mintLiq (token: MockBypassERC20, base: MockBypassERC20, dex: CrocSwapDex, query: QueryHelper): Promise<void> {
     let baseDecs = await base.decimals()
     let quoteDecs = await token.decimals()
 
@@ -85,7 +85,7 @@ async function mintLiq (token: MockERC20, base: MockERC20, dex: CrocSwapDex, que
     }
 }
 
-async function displayPrice (token: MockERC20, base: MockERC20, query: QueryHelper): Promise<void> {
+async function displayPrice (token: MockBypassERC20, base: MockBypassERC20, query: QueryHelper): Promise<void> {
     let baseDecs = await base.decimals()
     let quoteDecs = await token.decimals()
     let deflator = Math.pow(10, baseDecs-quoteDecs)
@@ -120,44 +120,51 @@ async function inflate() {
 
     factory = await ethers.getContractFactory("QueryHelper");
     //let query = factory.attach("0xa513E6E4b8f2a923D98304ec87F64353C4D5C853") as QueryHelper
-    let query = factory.attach("0x21E85d6C75a99B132e08dBDdB3166b2550D9e3b6") as QueryHelper
+    //let query = factory.attach("0x21E85d6C75a99B132e08dBDdB3166b2550D9e3b6") as QueryHelper
+    let query = factory.attach("0xd40CBB866A7f0b1e36132a86DDfe1a8bA8492286") as QueryHelper
 
-    /*let usdc = await deployToken("USDC", 6)
+    let avax = await deployToken("AVAX", 12)
+    let usdc = await deployToken("USDC", 6)
     let wbtc = await deployToken("WBTC", 8)
     let usdt = await deployToken("USDT", 6)
     let mkr = await deployToken("MKR", 18)
-    let shib = await deployToken("SHIB", 18)*/
+    let shib = await deployToken("SHIB", 12)
 
-    factory = await ethers.getContractFactory("MockERC20")
-    let usdc = factory.attach("0x83e77C197E744D21810A1f970cD24A246E0932a1") as MockERC20
-    let wbtc = factory.attach("0xccea4Dfe9F0dBCCf6357b935846bF67778167D99") as MockERC20
-    let usdt = factory.attach("0xe41BC7F1bD07a1A6651B6Fad2520715c1827F692") as MockERC20
-    let mkr = factory.attach("0x1440186D311F764Ce7e3C2164E2Dff4cf1826A97") as MockERC20
-    let shib = factory.attach("0x49f4B51EB3029024335ea30E2f46c3F5D35c40a8") as MockERC20
+    factory = await ethers.getContractFactory("MockBypassERC20")
+    /*let usdc = factory.attach("0x83e77C197E744D21810A1f970cD24A246E0932a1") as MockBypassERC20
+    let wbtc = factory.attach("0xccea4Dfe9F0dBCCf6357b935846bF67778167D99") as MockBypassERC20
+    let usdt = factory.attach("0xe41BC7F1bD07a1A6651B6Fad2520715c1827F692") as MockBypassERC20
+    let mkr = factory.attach("0x1440186D311F764Ce7e3C2164E2Dff4cf1826A97") as MockBypassERC20
+    let shib = factory.attach("0x49f4B51EB3029024335ea30E2f46c3F5D35c40a8") as MockBypassERC20*/
 
     let tokens = [usdc, wbtc, usdt, mkr, shib]
     let quotables = [wbtc, usdt, mkr, shib]
 
+    await fundToken(avax, trader, dex.address)
     await fundToken(usdc, trader, dex.address)
     await fundToken(wbtc, trader, dex.address)
     await fundToken(usdt, trader, dex.address)
     await fundToken(mkr, trader, dex.address)
     await fundToken(shib, trader, dex.address)
 
-    /*await createPool(usdt, usdc, 1.0, dex)
-    await createPool(wbtc, usdc, 65000.0, dex)
-    await createPool(mkr, usdc, 3340.0, dex)
-    await createPool(shib, usdc, 0.000089, dex)*/
+    await createPool(usdc, avax, 0.001, dex)
+    await createPool(usdt, avax, 0.001, dex)
+    await createPool(wbtc, avax, 650.0, dex)
+    await createPool(mkr, avax, 33.4, dex)
+    await createPool(shib, avax, 0.002, dex)
+    //await createPool(shib, usdc, 0.000089, dex)
 
-    /*await mintLiq(wbtc, usdc, dex, query)
-    await mintLiq(usdt, usdc, dex, query)
-    await mintLiq(mkr, usdc, dex, query)
-    await mintLiq(shib, usdc, dex, query)
+    await mintLiq(wbtc, avax, dex, query)
+    await mintLiq(usdt, avax, dex, query)
+    await mintLiq(usdc, avax, dex, query)
+    await mintLiq(mkr, avax, dex, query)
+    await mintLiq(shib, avax, dex, query)
     
-    await displayPrice(wbtc, usdc, query)
-    await displayPrice(usdt, usdc, query)
-    await displayPrice(mkr, usdc, query)
-    await displayPrice(shib, usdc, query)*/
+    await displayPrice(wbtc, avax, query)
+    await displayPrice(usdt, avax, query)
+    await displayPrice(usdc, avax, query)
+    await displayPrice(mkr, avax, query)
+    await displayPrice(shib, avax, query)
 }
 
 inflate()
