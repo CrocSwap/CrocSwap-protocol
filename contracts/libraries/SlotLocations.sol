fb4a0302c7e19c32e1c75868adacae2d6e5fc45e
Douglas R Colkitt <douglas.colkitt@gmail.com>
2022-02-25 19:32:09 +0000

    Fix slot locations

--- contracts/libraries/SlotLocations.sol
+++ contracts/libraries/SlotLocations.sol
@@ -5,6 +5,8 @@ pragma solidity >=0.8.4;
 library CrocSlots {
     uint constant public AUTHORITY_SLOT = 0;
     uint constant public FEE_MAP_SLOT = 77;
+    uint constant public POS_MAP_SLOT = 78;
+    uint constant public AMB_MAP_SLOT = 79;
     uint constant public AGENT_MAP_SLOT = 80;
 
     uint constant public AGENT_DEBIT_OFFSET = 0x100;
