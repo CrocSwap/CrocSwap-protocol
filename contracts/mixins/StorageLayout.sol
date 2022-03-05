44a4fa5b6c2221ffc8f4e44fcfe8c8c607de0b0a
Douglas R Colkitt <douglas.colkitt@gmail.com>
2022-03-05 05:22:12 +0000

    Consolidate the SlotLocations library into the StorageLayout.sol as its tightly bound to the StorageLayout mixin

--- contracts/mixins/StorageLayout.sol
+++ contracts/mixins/StorageLayout.sol
@@ -138,3 +138,17 @@ contract StoragePrototypes is StorageLayout {
     AmbientPosition amb_;
     BookLevel lvl_;
 }
+
+library CrocSlots {
+    uint constant public AUTHORITY_SLOT = 0;
+    uint constant public LVL_MAP_SLOT = 65538;
+    uint constant public FEE_MAP_SLOT = 65546;
+    uint constant public POS_MAP_SLOT = 65547;
+    uint constant public AMB_MAP_SLOT = 65548;
+    uint constant public AGENT_MAP_SLOT = 65549;
+    uint constant public CURVE_MAP_SLOT = 65550;
+    uint constant public BAL_MAP_SLOT = 65551;
+
+    uint constant public AGENT_DEBIT_OFFSET = 0x100;
+    uint constant public AGENT_BURN_OFFSET = 0x1;
+}
