610fb8f883b6741c6045850fe5c27ee1c0d287e3
Douglas R Colkitt <douglas.colkitt@gmail.com>
2022-03-10 00:08:17 +0000

    Add top-level comments on croc-specific interfaces

--- contracts/interfaces/ICrocPermitOracle.sol
+++ contracts/interfaces/ICrocPermitOracle.sol
@@ -11,14 +11,20 @@ interface ICrocPermitOracle {
      *          action on the pool.
      *
      * @param user The address of the caller to the contract.
-     * @param base The base-side token in the pair.
+     * @param sender The value of msg.sender for the caller of the action. Will either
+     *               be same as user, the calling router, or the off-chain relayer.
      * @param quote The quote-side token in the pair.
      * @param ambient The ambient liquidity directive for the pool action (possibly zero)
      * @param swap    The swap directive for the pool (possibly zero)
      * @param concs   The concentrated liquidity directives for the pool (possibly empty)
+     * @param poolFee The effective pool fee set for the swap (either the base fee or the
+     *                base fee plus user tip).
      *
-     * @returns       Returns true if action is permitted. If false, CrocSwap will revert
-     *                the transaction. */
+     * @returns       If zero, the transaction fails. If non-zero, the user will pay
+     *                a swap fee equivalent to @poolFee minus this value. This allows
+     *                a permit oracle to disriminate swap fees on a per user basis. (A
+     *                value of 1 is economically meaningless at .0001%, and can be used
+     *                for the no discount scenario.) */
     function checkApprovedForCrocPool (address user, address sender,
                                        address base, address quote,
                                        Directives.AmbientDirective calldata ambient,
@@ -30,15 +36,22 @@ interface ICrocPermitOracle {
     /* @notice Verifies whether a given user is permissioned to perform a swap on the pool
      *
      * @param user The address of the caller to the contract.
+     * @param sender The value of msg.sender for the caller of the action. Will either
+     *               be same as user, the calling router, or the off-chain relayer.
      * @param base The base-side token in the pair.
      * @param quote The quote-side token in the pair.
      * @param isBuy  If true, the swapper is paying base and receiving quote
      * @param inBaseQty  If true, the qty is denominated in the base token side.
      * @param qty        The full qty on the swap request (could possibly be lower if user
      *                   hits limit price.
+     * @param poolFee The effective pool fee set for the swap (either the base fee or the
+     *                base fee plus user tip).
      *
-     * @returns       Returns true if action is permitted. If false, CrocSwap will revert
-     *                the transaction. */
+     * @returns       If zero, the transaction fails. If non-zero, the user will pay
+     *                a swap fee equivalent to @poolFee minus this value. This allows
+     *                a permit oracle to disriminate swap fees on a per user basis. (A
+     *                value of 1 is economically meaningless at .0001%, and can be used
+     *                for the no discount scenario.) */
     function checkApprovedForCrocSwap (address user, address sender,
                                        address base, address quote,
                                        bool isBuy, bool inBaseQty, uint128 qty,
@@ -49,6 +62,8 @@ interface ICrocPermitOracle {
      *         on the pool.
      *
      * @param user The address of the caller to the contract.
+     * @param sender The value of msg.sender for the caller of the action. Will either
+     *               be same as user, the calling router, or the off-chain relayer.
      * @param base The base-side token in the pair.
      * @param quote The quote-side token in the pair.
      * @param bidTick  The tick index of the lower side of the range (0 if ambient)
@@ -67,6 +82,8 @@ interface ICrocPermitOracle {
      *         on the pool.
      *
      * @param user The address of the caller to the contract.
+     * @param sender The value of msg.sender for the caller of the action. Will either
+     *               be same as user, the calling router, or the off-chain relayer.
      * @param base The base-side token in the pair.
      * @param quote The quote-side token in the pair.
      * @param bidTick  The tick index of the lower side of the range (0 if ambient)
@@ -81,6 +98,18 @@ interface ICrocPermitOracle {
                                        int24 bidTick, int24 askTick, uint128 liq)
         external returns (bool);
 
+    /* @notice Verifies whether a given user is permissioned to initialize a pool
+     *         attached to this oracle.
+     *
+     * @param user The address of the caller to the contract.
+     * @param sender The value of msg.sender for the caller of the action. Will either
+     *               be same as user, the calling router, or the off-chain relayer.
+     * @param base The base-side token in the pair.
+     * @param quote The quote-side token in the pair.
+     * @param poolIdx The Croc-specific pool type index the pool is being created on.
+     *
+     * @returns       Returns true if action is permitted. If false, CrocSwap will revert
+     *                the transaction, and pool will not be initialized. */
     function checkApprovedForCrocInit (address user, address sender,
                                        address base, address quote, uint256 poolIdx)
         external returns (bool);
