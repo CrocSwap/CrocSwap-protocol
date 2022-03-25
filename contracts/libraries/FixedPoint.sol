e566513d439a80d8f3bd1d8129d98af7d987e28b
Douglas R Colkitt <douglas.colkitt@gmail.com>
2022-03-25 19:05:27 +0000

    Add unchecked blocks where math is guaranteed safe in FixedPoint

--- contracts/libraries/FixedPoint.sol
+++ contracts/libraries/FixedPoint.sol
@@ -11,26 +11,36 @@ library FixedPoint {
 
     /* @notice Multiplex two Q64.64 numbers by each other. */
     function mulQ64 (uint128 x, uint128 y) internal pure returns (uint192) {
+        unchecked {
         return uint192((uint256(x) * uint256(y)) >> 64);
+        }
     }
 
     /* @notice Divides one Q64.64 number by another. */
     function divQ64 (uint128 x, uint128 y) internal pure returns (uint192) {
+        unchecked {
         return (uint192(x) << 64) / y;
+        }
     }
 
     /* @notice Dives a Q64.64 numerator by the square of another Q64.64 fixed point. */
     function divSqQ64 (uint128 x, uint128 y) internal pure returns (uint256) {
+        unchecked {
         return (uint192(x) << 64) / (uint256(y)*uint256(y));
+        }
     }
 
     /* @notice Multiplies a Q64.64 by a Q16.48. */
     function mulQ48 (uint128 x, uint64 y) internal pure returns (uint144) {
+        unchecked {
         return uint128((uint256(x) * uint256(y)) >> 48);
+        }
     }
 
     /* @notice Takes the reciprocal of a Q64.64 number. */
     function recipQ64 (uint128 x) internal pure returns (uint128) {
+        unchecked {
         return uint128(FixedPoint.Q128 / x);
+        }
     }
 }
