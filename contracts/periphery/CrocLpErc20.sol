47995d6cbf623b36cebb74ed5ebe08cc46635b2c
Douglas R Colkitt <douglas.colkitt@gmail.com>
2022-05-01 02:41:54 +0000

    Add an ERC20 compliant ambient liquidity LP conduit

---
+++ contracts/periphery/CrocLpErc20.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: Unlicensed
+
+pragma solidity >=0.5.0;
+
+import "../libraries/PoolSpecs.sol";
+import "../interfaces/ICrocLpConduit.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "hardhat/console.sol";
+
+contract CrocLpErc20 is ERC20, ICrocLpConduit {
+
+    bytes32 public immutable poolHash;
+    address public immutable baseToken;
+    address public immutable quoteToken;
+    uint256 public immutable poolType;
+    
+    constructor (address base, address quote, uint256 poolIdx)
+        ERC20 ("Croc Ambient LP ERC20 Token", "LP-CrocAmb") {
+        baseToken = base;
+        quoteToken = quote;
+        poolType = poolIdx;
+        poolHash = PoolSpecs.encodeKey(base, quote, poolIdx);
+    }
+    
+    function depositCrocLiq (address sender, bytes32 pool,
+                             int24 lowerTick, int24 upperTick, uint128 seeds,
+                             uint64) public override returns (bool) {
+        require(pool == poolHash, "Wrong pool");
+        require(lowerTick == 0 && upperTick == 0, "Non-Ambient LP Deposit");
+        _mint(sender, seeds);
+        return true;
+    }
+
+    function withdrawCrocLiq (address sender, bytes32 pool,
+                              int24 lowerTick, int24 upperTick, uint128 seeds,
+                              uint64) public override returns (bool) {
+        require(pool == poolHash, "Wrong pool");
+        require(lowerTick == 0 && upperTick == 0, "Non-Ambient LP Deposit");
+        _burn(sender, seeds);
+        return true;
+    }
+
+}
