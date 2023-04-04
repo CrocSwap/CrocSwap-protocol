61c45c4e21a8a89320f6dd91330500279164b64a
Douglas R. Colkitt <douglas.colkitt@gmail.com>
2023-04-04 17:20:34 -0400

    Add more networks to deploy script

--- misc/scripts/deploy.ts
+++ misc/scripts/deploy.ts
@@ -1,7 +1,7 @@
 
 import { CrocSwapDexSeed } from '../../typechain/CrocSwapDexSeed';
 import { ethers } from 'hardhat';
-import { ContractFactory, BytesLike, BigNumber } from 'ethers';
+import { ContractFactory, BytesLike, BigNumber, Signer } from 'ethers';
 import { JsonRpcProvider } from '@ethersproject/providers';
 import { toSqrtPrice, fromSqrtPrice, MIN_PRICE, MAX_PRICE, MIN_TICK, ZERO_ADDR } from '../../test/FixedPoint';
 import { MockERC20 } from '../../typechain/MockERC20';
@@ -17,9 +17,23 @@ import { CrocPolicy } from '../../typechain/CrocPolicy';
 import { CrocQuery } from '../../typechain/CrocQuery';
 import { CrocShell } from '../../typechain/CrocShell';
 import { HotPath } from '../../typechain/HotPath';
-import { CrocImpact, KnockoutFlagPath, KnockoutLiqPath } from '../../typechain';
+import { BootPath, CrocImpact, KnockoutFlagPath, KnockoutLiqPath } from '../../typechain';
 
-let override = { gasPrice: BigNumber.from("10").pow(9).mul(2), gasLimit: 6000000 }
+interface CrocAddrs {
+    dex: string | undefined,
+    cold: string | undefined,
+    warm: string | undefined,
+    long: string | undefined,
+    micro: string | undefined,
+    hot: string | undefined,
+    knockout: string | undefined,
+    koCross: string | undefined,
+    policy: string | undefined,
+    query: string | undefined,
+    impact: string | undefined,
+    shell: string | undefined
+  }
+
 
 /* Ropsten */
 /*let addrs = {
@@ -48,7 +62,7 @@ let override = { gasPrice: BigNumber.from("10").pow(9).mul(2), gasLimit: 6000000
 }*/
 
 // Goerli
-let addrs = {
+/*let addrs = {
     dex: "0xfafcd1f5530827e7398b6d3c509f450b1b24a209",
     cold: "0xb2ae163293c82dcf36b0ce704591edc2f9e2608d",
     warm: "0x01B180D35125D31B4057d9ac7F46687dA1cAEFab",
@@ -61,7 +75,57 @@ let addrs = {
     query: "0x93a4baFDd49dB0e06f3F3f9FddC1A67792F47518", 
     impact: "0x142BE02F2A3A27ecD6e2f18a43c2C234F372C831",
     shell: "0xdf2a97ae85e8ce33ad20ad2d3960fd92e8079861"
-}
+}*/
+
+// Arbitrum Goerli
+/*let addrs = {
+    dex: '0x9EA4B2f9b1572eD3aC46b402d9Ba9153821033C6',
+    cold: '0xb2aE163293C82DCF36b0cE704591eDC2f9E2608D',
+    warm: '0x01B180D35125D31B4057d9ac7F46687dA1cAEFab',
+    long: '0x66D34E1486d0bAd1A8ceD5a8505a73d0CFD41a0A',
+    micro: '0x323172539B1B0D9eDDFFBd0318C4d6Ab45292843',
+    hot: '0x141E224f461a85006b2EF051a7C1c290E449202A',
+    knockout: '0xfAfcD1f5530827e7398B6D3C509f450b1b24a209',
+    koCross: '0xAA391eE82F0C6b406E98cCd76d637CaC2f712228',
+    policy: '0xDf2a97aE85e8Ce33ad20Ad2D3960fD92E8079861',
+    query: '0x6291Aa5812FF75412Cf3F3258447139653A9a209',
+    impact: '0x5afc7599A4b659C5c628fBC212012B68F3b5D41C',
+    shell: '0x157EcDcCE75f24635cEB7FF9F2ac9BFf3ebF9733'
+  }*/
+
+
+
+// Mumbai
+/*let addrs: CrocAddrs = {
+    dex: '0x0bE8385D8CDDE8FACb54cF52FEd856D6C37Bb8e3',
+    cold: '0x8e5a7ca8D9Ef7f9F6bedabCf9c21c539D6074bD4',
+    warm: '0xbCb14ced50469E9F6B5E23527E7cdE0d96f2f0cf',
+    long: '0x8D8Fd9A5CDF3b7238223E17e4e4c66D4500a13C6',
+    micro: '0xd40CBB866A7f0b1e36132a86DDfe1a8bA8492286',
+    hot: '0x6c53969F9273560F393a8BcbFA40906E7B51b1B2',
+    knockout: '0x66B5b7f1F5604FC33aF247D59a7938369B37358F',
+    koCross: '0x0C4BA0D85b6a93ae8746dBE4Bd1E9499D8e61999',
+    policy: '0xCb0403d581C2BB794F5734D9c63bE31Bf18892c3',
+    query: '0x6E6e2526cE1951576F452053Df1C5D11336738de',
+    impact: '0x4d054Cb42B2AC113FFe542da7CE4A64Cf570898D',
+    shell: '0x57f8908f340D522ca8B6E2E89d31b4eEF9B1779B'
+  }*/
+
+  // Fuji
+  let addrs: CrocAddrs = {
+    dex: undefined,
+    cold: undefined,
+    warm: undefined,
+    long: undefined,
+    micro: undefined,
+    hot: undefined,
+    knockout: undefined,
+    koCross: undefined,
+    policy: undefined,
+    query: undefined,
+    impact: undefined,
+    shell: undefined
+  }
 
 // Ropsten
 /*let tokens = {
@@ -84,115 +148,182 @@ let tokens = {
     usdc: "0xD87Ba7A50B2E7E660f678A895E4B72E7CB4CCd9C"
 }
 
-async function deploy() {
-    let authority = (await ethers.getSigners())[0]
+const POOL_IDX = 36000
 
-    let abi = new ethers.utils.AbiCoder()
-    let cmd;
+const BOOT_PROXY_IDX = 0;
+const SWAP_PROXY_IDX = 1;
+const LP_PROXY_IDX = 2;
+const COLD_PROXY_IDX = 3;
+const LONG_PROXY_IDX = 4;
+const MICRO_PROXY_IDX = 5;
+const MULTICALL_PROXY_IDX = 6;
+const KNOCKOUT_LP_PROXY_IDX = 7;
+const FLAG_CROSS_PROXY_IDX = 3500;
+const SAFE_MODE_PROXY_PATH = 9999;
+
+let abi = new ethers.utils.AbiCoder()
+const override = { gasLimit: 6000000 }
+
+async function createDexContracts(): Promise<CrocSwapDex> {
     let factory
-    let tx;
-
-    console.log("Deploying with the following addresses...")
-    console.log("Protocol Authority: ", await authority.address)
 
     factory = await ethers.getContractFactory("WarmPath")
     let warmPath = addrs.warm ? factory.attach(addrs.warm) :
         await factory.deploy(override) as WarmPath
-    
+    addrs.warm = warmPath.address
+        
     factory = await ethers.getContractFactory("LongPath")
     let longPath = addrs.long ? factory.attach(addrs.long) :
         await factory.deploy(override) as LongPath
+    addrs.long = longPath.address
     
     factory = await ethers.getContractFactory("MicroPaths")
     let microPath = addrs.micro ? factory.attach(addrs.micro) :
         await factory.deploy(override) as MicroPaths
+    addrs.micro = microPath.address
 
     factory = await ethers.getContractFactory("ColdPath")
     let coldPath = addrs.cold ? factory.attach(addrs.cold) :
         await factory.deploy(override) as ColdPath
+    addrs.cold = coldPath.address
 
     factory = await ethers.getContractFactory("HotProxy")
     let hotPath = addrs.hot ? factory.attach(addrs.hot) :
         await factory.deploy(override) as HotPath
+    addrs.hot = hotPath.address
 
     factory = await ethers.getContractFactory("KnockoutLiqPath")
     let knockoutPath = addrs.knockout ? factory.attach(addrs.knockout) :
         await factory.deploy(override) as KnockoutLiqPath    
+    addrs.knockout = knockoutPath.address
 
     factory = await ethers.getContractFactory("KnockoutFlagPath")
     let crossPath = addrs.koCross ? factory.attach(addrs.koCross) :
         await factory.deploy(override) as KnockoutFlagPath    
+    addrs.koCross = crossPath.address
         
     factory = await ethers.getContractFactory("CrocSwapDex")
-    let dex = addrs.dex ? factory.attach(addrs.dex) :
-        await factory.deploy(authority.address, coldPath.address, override) as CrocSwapDex
-            
+    let dex = (addrs.dex ? factory.attach(addrs.dex) :
+        await factory.deploy(override)) as CrocSwapDex
+    addrs.dex = dex.address
+
+    console.log(addrs)
+    return dex
+}
+
+async function createPeripheryContracts (dexAddr: string): Promise<CrocPolicy> {
+    let factory;
+
     factory = await ethers.getContractFactory("CrocPolicy")
     let policy = (addrs.policy ? factory.attach(addrs.policy) :
-        await factory.deploy(dex.address, await authority.getAddress(),
-        await authority.getAddress(), await authority.getAddress())) as CrocPolicy
+        await factory.deploy(dexAddr)) as CrocPolicy
+    addrs.policy = policy.address
 
     factory = await ethers.getContractFactory("CrocQuery")
     let query = (addrs.query ? factory.attach(addrs.query) :
-        await factory.deploy(dex.address, override)) as CrocQuery
+        await factory.deploy(dexAddr, override)) as CrocQuery
+    addrs.query = query.address
 
     factory = await ethers.getContractFactory("CrocImpact")
     let impact = (addrs.impact ? factory.attach(addrs.impact) :
-        await factory.deploy(dex.address, override)) as CrocImpact
+        await factory.deploy(dexAddr, override)) as CrocImpact
+    addrs.impact = impact.address
         
-    console.log(impact.address)
-    
     factory = await ethers.getContractFactory("CrocShell")
     let shell = (addrs.shell ? factory.attach(addrs.shell) :
         await factory.deploy(override)) as CrocShell
+    addrs.shell = shell.address
 
-    console.log({ warm: warmPath.addrs, long: longPath.addrs, micro: microPath.addrs, hot: hotPath.addrs, 
-        dex: dex.addrs, policy: policy.addrs, query: query.addrs, shell: shell.addrs})
+    console.log(addrs)
+    return policy 
+}
 
-    factory = await ethers.getContractFactory("MockERC20")
+async function installPolicy (dex: CrocSwapDex) {
+    let authCmd = abi.encode(["uint8", "address"], [20, addrs.policy])
+    let tx = await dex.protocolCmd(COLD_PROXY_IDX, authCmd, true, override);
+    await tx.wait()
+}
+
+async function installSidecars (dex: CrocSwapDex) {
+
+    let abi = new ethers.utils.AbiCoder()
+    let tx
+    let cmd;
+
+    cmd = abi.encode(["uint8", "address", "uint16"], [21, addrs.cold, COLD_PROXY_IDX])
+    tx = await dex.protocolCmd(BOOT_PROXY_IDX, cmd, true)
+    await tx
+
+    cmd = abi.encode(["uint8", "address", "uint16"], [21, addrs.warm, LP_PROXY_IDX])
+    tx = await dex.protocolCmd(BOOT_PROXY_IDX, cmd, true)
+    await tx
+
+    cmd = abi.encode(["uint8", "address", "uint16"], [21, addrs.hot, SWAP_PROXY_IDX])
+    tx = await dex.protocolCmd(BOOT_PROXY_IDX, cmd, true)
+    await tx
+
+    cmd = abi.encode(["uint8", "address", "uint16"], [21, addrs.long, LONG_PROXY_IDX])
+    tx = await dex.protocolCmd(BOOT_PROXY_IDX, cmd, true)
+    await tx
+
+    cmd = abi.encode(["uint8", "address", "uint16"], [21, addrs.micro, MICRO_PROXY_IDX])
+    tx = await dex.protocolCmd(BOOT_PROXY_IDX, cmd, true)
+    await tx
+
+    cmd = abi.encode(["uint8", "address", "uint16"], [21, addrs.knockout, KNOCKOUT_LP_PROXY_IDX])
+    tx = await dex.protocolCmd(BOOT_PROXY_IDX, cmd, true)
+    await tx
+
+    cmd = abi.encode(["uint8", "address", "uint16"], [21, addrs.koCross, FLAG_CROSS_PROXY_IDX])
+    tx = await dex.protocolCmd(BOOT_PROXY_IDX, cmd, true)
+    await tx
+}
+
+async function initPoolTemplate (policy: CrocPolicy) {
+    const POOL_INIT_LIQ = 10000
+    const FEE_BPS = 28
+    const TICK_SIZE = 16
+    const JIT_THRESH = 3
+
+    const KNOCKOUT_ON_FLAG = 32
+    const KNOCKOUT_TICKS_FLAG = 4 // 16 ticks
+    const knockoutFlag = KNOCKOUT_ON_FLAG + KNOCKOUT_TICKS_FLAG
+
+    if (addrs.dex) { 
+        let setPoolLiqCmd = abi.encode(["uint8", "uint128"], [112, POOL_INIT_LIQ])
+        let tx = await policy.treasuryResolution(addrs.dex, COLD_PROXY_IDX, setPoolLiqCmd, false, override)
+        await tx.wait()
+
+        let templateCmd = abi.encode(["uint8", "uint256", "uint16", "uint16", "uint8", "uint8", "uint8"],
+            [110, POOL_IDX, FEE_BPS * 100, TICK_SIZE, JIT_THRESH, knockoutFlag, 0])
+        tx = await policy.opsResolution(addrs.dex, COLD_PROXY_IDX, templateCmd, override)
+        await tx.wait()
+    }
+}
+
+async function deploy() {
+    let authority = (await ethers.getSigners())[0]
+
+    console.log("Deploying with the following addresses...")
+    console.log("Protocol Authority: ", await authority.address)
+
+    let dex = await createDexContracts()
+    let policy = await createPeripheryContracts(dex.address)
+  
+    /*await installSidecars(dex)
+    await installPolicy(dex)*/
+
+    await initPoolTemplate(policy)
+  
+    /*factory = await ethers.getContractFactory("MockERC20")
     let dai = factory.attach(tokens.dai) as MockERC20
-    let usdc = factory.attach(tokens.usdc) as MockERC20
+    let usdc = factory.attach(tokens.usdc) as MockERC20*/
 
     /*tx = await dai.approve(dex.address, BigNumber.from(10).pow(36))
     await tx.wait()
 
     tx = await usdc.approve(dex.address, BigNumber.from(10).pow(36))
     await tx.wait()*/
-
-    /*let authCmd = abi.encode(["uint8", "address"], [20, policy.address])
-    tx = await dex.protocolCmd(0, authCmd, true, override);
-    await tx.wait()
-
-    let upCmd = abi.encode(["uint8", "address", "uint16"], [21, warmPath.address, 2])
-    tx = await policy.treasuryResolution(dex.address, 0, upCmd, true, override);
-    await tx.wait()
-
-    upCmd = abi.encode(["uint8", "address", "uint16"], [21, longPath.address, 4])
-    tx = await policy.treasuryResolution(dex.address, 0, upCmd, true, override);
-    await tx.wait()
-
-    upCmd = abi.encode(["uint8", "address", "uint16"], [21, microPath.address, 5])
-    tx = await policy.treasuryResolution(dex.address, 0, upCmd, true, override);
-    await tx.wait() 
-
-    let upCmd = abi.encode(["uint8", "address", "uint16"], [21, knockoutPath.address, 7])
-    tx = await policy.treasuryResolution(dex.address, 0, upCmd, true, override);
-    await tx.wait()
-
-    upCmd = abi.encode(["uint8", "address", "uint16"], [21, crossPath.address, 3500])
-    tx = await policy.treasuryResolution(dex.address, 0, upCmd, true, override);
-    await tx.wait()*/
-
-    /*let setPoolLiqCmd = abi.encode(["uint8", "uint128"], [112, 10000])
-    tx = await policy.treasuryResolution(dex.address, 0, setPoolLiqCmd, false)
-    await tx.wait()
-
-    let templateCmd = abi.encode(["uint8", "uint256", "uint16", "uint16", "uint8", "uint8", "uint8"],
-        [110, 36000, 500, 64, 5, 64, 0])
-    tx = await policy.opsResolution(dex.address, 0, templateCmd)
-    await tx.wait()
-
-    return*/
 
     /*console.log("Q")
     let initPoolCmd = abi.encode(["uint8", "address", "address", "uint256", "uint128"],
@@ -205,23 +336,6 @@ async function deploy() {
         [71, tokens.usdc, tokens.dai, 36000, toSqrtPrice(Math.pow(10, -12))])
     tx = await dex.userCmd(0, initUsdcCmd, { gasLimit: 6000000})
     console.log(tx)
-    await tx.wait()*/
-
-    // Enable knockout liquidity
-    /*const knockoutFlag = 32 + 6 // Enabled, on grid, 32-ticks wide
-    let reviseCmd = abi.encode(["uint8", "address", "address", "uint256", "uint16", "uint16", "uint8", "uint8"],
-        [111, tokens.eth, tokens.dai, 36000, 1000, 64, 5, knockoutFlag])
-    tx = await policy.treasuryResolution(dex.address, 0, reviseCmd, false)
-    await tx.wait()*/
-
-    /*reviseCmd = abi.encode(["uint8", "address", "address", "uint256", "uint16", "uint16", "uint8", "uint8"],
-        [111, tokens.eth, tokens.usdc, 36000, 500, 64, 5, knockoutFlag])
-    tx = await policy.treasuryResolution(dex.address, 0, reviseCmd, false)
-    await tx.wait()
-
-    reviseCmd = abi.encode(["uint8", "address", "address", "uint256", "uint16", "uint16", "uint8", "uint8"],
-        [111, tokens.usdc, tokens.dai, 36000, 500, 64, 5, knockoutFlag])
-    tx = await policy.treasuryResolution(dex.address, 0, reviseCmd, false)
     await tx.wait()*/
 
     /*let mintCmd = abi.encode(["uint8", "address", "address", "uint256", "int24", "int24", "uint128", "uint128", "uint128", "uint8", "address"],
