4320e3ec5d517bb92475d506cb05d5e17d1d4843
ocnc <anocpublic@gmail.com>
2024-02-08 15:57:34 -0500

    pog preview

--- contracts/periphery/BeraCrocMultiSwap.sol
+++ contracts/periphery/BeraCrocMultiSwap.sol
@@ -1,14 +1,62 @@
 pragma solidity 0.8.19;
 
 import "../CrocSwapDex.sol";
+import "../lens/CrocImpact.sol";
 import "../libraries/SwapHelpers.sol";
 import "../interfaces/IERC20Minimal.sol";
 
 contract BeraCrocMultiSwap {
-    CrocSwapDex public crocSwapDex;
+    CrocSwapDex public immutable crocSwapDex;
+    CrocImpact private immutable crocImpact;
 
-    constructor(address _crocSwapDex) {
+    constructor(address _crocSwapDex, address _crocImpact) {
         crocSwapDex = CrocSwapDex(_crocSwapDex);
+        crocImpact = CrocImpact(_crocImpact);
+    }
+
+    /* @notice Preview a series of swaps between multiple pools.
+     *
+     * @dev A convenience method for previewing a series of swaps in sequence. This is
+     *      to be used in conjunction with some form of an off-chain router as the input
+     *      arguments assume the user already knows the exact sequence of swaps to
+     *      perform.
+     *  
+     * @param steps The series of swap steps to be performed in sequence.
+     * @return out the amount to be received from the multiswap. */
+    function previewMultiSwap(
+        SwapHelpers.SwapStep[] memory _steps,
+            uint128 _amount
+    ) external view returns (uint128 out) {
+        require(_steps.length != 0, "No steps provided");
+        SwapHelpers.SwapStep memory initStep = _steps[0];
+        uint128 quantity = _amount;
+        address inputAsset;
+        initStep.isBuy ? inputAsset = initStep.base : inputAsset = initStep.quote;
+        for (uint256 i=0; i < _steps.length; ) {
+            SwapHelpers.SwapStep memory step = _steps[i];
+            address swapAsset;
+            step.isBuy ? swapAsset = step.base : swapAsset = step.quote;
+            require(inputAsset == swapAsset, "Invalid swap sequence");
+            if (step.isBuy) {
+                // We use the max uint128 as the limit price to ensure the swap executes
+                // Given that we have full range liquidity, there is no min limit price
+                // Slippage can be controlled by the minOut parameter
+                (, int128 quoteFlow,) = crocImpact.calcImpact(step.base, step.quote, step.poolIdx,
+                step.isBuy, true, quantity, 0, type(uint128).max);
+                // Received amount is always negative
+                quantity = uint128(-quoteFlow);
+                inputAsset = step.quote;
+            } else {
+                // Limit price is 0 here for the inverse reason above
+                (int128 baseFlow,,) = crocImpact.calcImpact(step.base, step.quote, step.poolIdx,
+                step.isBuy, false, quantity, 0, 0);
+                // Received amount is always negative
+                quantity = uint128(-baseFlow);
+                inputAsset = step.base;
+            }
+            unchecked { i++; }
+        }
+        return quantity;
     }
 
     /* @notice Performs a series of swaps between multiple pools.
