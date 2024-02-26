a936c68abf781a1d755ba82182d8bd8b2f3a19b5
Douglas R. Colkitt <douglas.colkitt@gmail.com>
2024-02-25 19:17:09 -0500

    Add periphery contracts for ABI compatible swap() method that wraps call to userCmd() on swap

--- test/TestGas.proxy.ts
+++ test/TestGas.proxy.ts
@@ -60,4 +60,26 @@ describe('Gas Benchmarks Proxy Sidecars', () => {
         await test.testSwapOther(true, true, 1000, toSqrtPrice(1.1))
         await expectGas(test.testSwapOther(true, true, 1000, toSqrtPrice(1.1)), 116000)
     })
+
+    it("swap proxy router [@gas-test]", async() => {
+        await test.testUpgradeHotProxy(hotProxy.address, true)
+        await test.base.approve(await test.other, (await test.router).address, 100000000000)
+        await test.quote.approve(await test.other, (await test.router).address, 100000000000)
+ 
+        test.useSwapProxy.router = true
+        await test.testMint(-100, 100, 10000)
+        await test.testSwapOther(true, true, 1000, toSqrtPrice(1.1))
+        await expectGas(test.testSwapOther(true, true, 1000, toSqrtPrice(1.1)), 180000)
+    })
+
+    it("swap proxy bypass router [@gas-test]", async() => {
+        await test.testUpgradeHotProxy(hotProxy.address, true)
+        await test.testApproveRouter(await test.other, (await test.routerBypass).address, 
+            100000000, [test.HOT_PROXY])
+
+        test.useSwapProxy.bypass = true
+        await test.testMint(-100, 100, 10000)
+        await test.testSwapOther(true, true, 1000, toSqrtPrice(1.1))
+        await expectGas(test.testSwapOther(true, true, 1000, toSqrtPrice(1.1)), 126000)
+    })
 })
