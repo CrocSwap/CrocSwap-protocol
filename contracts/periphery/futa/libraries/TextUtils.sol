b7eabc4987ed24937fc3f0eb5fe2774c20a3317e
Douglas R. Colkitt <douglas.colkitt@gmail.com>
2024-12-18 17:15:22 -0500

    Fix compile time issues

--- contracts/periphery/futa/libraries/TextUtils.sol
+++ contracts/periphery/futa/libraries/TextUtils.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.19;
 
 library TextUtils {
     
-    function isUpperCase(string memory str) public pure returns (bool) {
+    function isUpperCase(string memory str) internal pure returns (bool) {
         bytes memory strBytes = bytes(str);
         for (uint i = 0; i < strBytes.length; i++) {
             bytes1 char = strBytes[i];
@@ -15,7 +15,7 @@ library TextUtils {
         return true;
     }
 
-    function makeCapitalized(string memory str) private pure returns (string memory) {
+    function makeCapitalized(string memory str) internal pure returns (string memory) {
         bytes memory strBytes = bytes(str);
         bytes memory result = new bytes(strBytes.length);
         for (uint i = 1; i < strBytes.length; i++) {
