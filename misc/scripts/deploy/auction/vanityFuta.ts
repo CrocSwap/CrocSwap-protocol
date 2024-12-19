18447ba92109a5afdcf5aa0ccf36042a3086d705
Douglas R. Colkitt <douglas.colkitt@gmail.com>
2024-12-18 21:51:39 -0500

    Debug Futa script

--- misc/scripts/deploy/auction/vanityFuta.ts
+++ misc/scripts/deploy/auction/vanityFuta.ts
@@ -11,19 +11,24 @@ import { AbiCoder } from '@ethersproject/abi';
 import { AuctionPath, ColdPath, CrocAuctionQuery, CrocDeployer, CrocPolicy, CrocQuery, CrocSwapDex, FutaLauncher } from '../../../../typechain';
 import { AUCTION_PROXY_IDX, BOOT_PROXY_IDX, COLD_PROXY_IDX } from '../../../constants/addrs';
 import { inflateAddr, initChain, refContract, traceContractTx, traceTxResp } from '../../../libs/chain';
+import { CrocAuctionAddrs } from '../../../types/addrs';
 
 const abi = new AbiCoder()
 
 async function vanityDeploy() {
     let { addrs, chainId, wallet: authority } = initChain()
 
-    const futa = await inflateAddr("FutaLauncher", addrs.futa?.futa || "", authority,
-        addrs.auction?.dex, addrs.dex, addrs.auction?.query, addrs.auction?.auctionPath, 420
+    const futa = await inflateAddr("FutaLauncher", addrs.auction?.futa || "", authority,
+        addrs.auction?.query, addrs.query, 420
     ) as FutaLauncher
 
-    addrs.auction.futa = futa.address
+    (addrs.auction as CrocAuctionAddrs).futa = futa.address
 
     console.log(`Updated addresses for ${chainId}`, addrs)
+
+    await traceContractTx(futa.setAuctionDuration(10), "Set Auction Duration")
+    await traceContractTx(futa.setAuctionSteps(10, 100), "Set Auction Steps")
+    await traceContractTx(futa.setTokenSupply(69_000_000_000, 13_800_000_000), "Set Token Supply")
 }
 
 vanityDeploy()
