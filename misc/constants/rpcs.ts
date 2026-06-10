953fc5558e12e9e5afbacdaf39b6e22762f7ee6c
Douglas R. Colkitt <douglas.colkitt@gmail.com>
2026-06-10 00:41:53 -0400

    Replace flaky llamarpc mainnet endpoint with publicnode

--- misc/constants/rpcs.ts
+++ misc/constants/rpcs.ts
@@ -3,5 +3,5 @@
 export const RPC_URLS = {
     'mock': "http://127.0.0.1:8545/",
     '0x5': 'https://goerli.infura.io/v3/cf3bc905d88d4f248c6be347adc8a1d8',
-    '0x1': 'https://eth.llamarpc.com'
+    '0x1': 'https://ethereum-rpc.publicnode.com'
 }
